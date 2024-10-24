5a9f73208f9f55811755e76a5accb6a3639d7887
danielbecee <dabe6612@gmail.com>
2024-10-24 15:51:32 +0200

    login

--- login.php
+++ login.php
@@ -1,39 +1,32 @@
 <?php
 // Iniciar sesión
 session_start();
-
 $_SESSION['error_username'] = $_SESSION['error_password'] = "";
 // Manejar el inicio de sesión
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Obtener y sanear los campos.
     $username = htmlspecialchars(trim($_POST['username']));
     $password = htmlspecialchars(trim($_POST['password']));
-
     // Validar el nombre de usuario.
     if (empty($username)) {
         $_SESSION['error_username'] = "El nombre de usuario es obligatorio.";
     }
-
     // Validar la contraseña.
     if (empty($password)) {
         $_SESSION['error_password'] = "La contraseña es obligatoria.";
     }
-
     // Redirigir a la misma página para mostrar los errores.
     if (isset($_SESSION['error_username']) || isset($_SESSION['error_password'])) {
         header("Location: index.php");
         exit();
     }
-
     try {
         include "./conexion.php";
         // $name = mysqli_real_escape_string($con, $_POST['nick']);
         // $pwd = mysqli_real_escape_string($con, $_POST['pwd']);
         $name = "Darckfer";
         $pwd = "qweQWE123";
-
         $sql = "SELECT * FROM tbl_usuarios WHERE nick_usu=?";
-
         $stmt = mysqli_stmt_init($con);
         if (mysqli_stmt_prepare($stmt, $sql)) {
             mysqli_stmt_bind_param($stmt, "s", $name);
@@ -57,7 +50,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         die();
     }
 }
-
 // Redirigir si accede a esta página sin POST
 header("Location: index.php");
 exit();
