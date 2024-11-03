f6e3ed35755ceabe1db4b6964cc008a6c4bade7a
Darckfer <juliocesarcarrillorocha@gmail.com>
2024-11-03 18:41:16 +0100

    buscador funcional

--- procesos_perfil/encontraramigo.php
+++ procesos_perfil/encontraramigo.php
@@ -1,6 +1,7 @@
 <?php
 
 include("./procesos_perfil/conexion.php");
+
 try {
     $id = $_SESSION['id_user'];
     $searchTerm = isset($_GET['search']) ? $_GET['search'] : '';
@@ -24,7 +25,7 @@ try {
 
     if (!empty($searchTerm)) {
         $searchPattern = '%' . $searchTerm . '%';
-        mysqli_stmt_bind_param($stmt, 'iiiss', $id, $id, $id, $id, $id, $searchPattern, $searchPattern);
+        mysqli_stmt_bind_param($stmt, 'iiiiss', $id,  $id, $id, $id, $searchPattern, $searchPattern);
     } else {
         mysqli_stmt_bind_param($stmt, 'iiii', $id, $id, $id, $id);
     }
@@ -37,28 +38,33 @@ try {
         <input type="submit" value="Buscar">
     </form>
 
-    <table>
-        <tbody>
-            <?php
-            foreach ($resultado as $fila) {
-            ?>
-                <tr style="text-align: left;">
-                    <!-- <?php echo $fila['id_usu']; ?> -->
-                    <td scope="row"> <?php echo $fila['nick_usu'] . " | "; ?>
-                        <?php echo $fila['nom_usu']; ?>
-                    </td>
-                    <td>
-                        <form action="./procesos_perfil/añadir_amigo.php" method="post">
-                            <input type="hidden" name="id" value="<?php echo $fila['id_usu']; ?>">
-                            <input type="submit" value="Añadir">
-                        </form>
-                    </td>
-                </tr>
-            <?php
-            }
-            ?>
-        </tbody>
-    </table>
+    <?php if (!empty($searchTerm) && mysqli_num_rows($resultado) > 0) : ?>
+        <table>
+            <tbody>
+                <?php
+                foreach ($resultado as $fila) {
+                ?>
+                    <tr style="text-align: left;">
+                        <!-- <?php echo $fila['id_usu']; ?> -->
+                        <td scope="row"> <?php echo $fila['nick_usu'] . " | "; ?>
+                            <?php echo $fila['nom_usu']; ?>
+                        </td>
+                        <td>
+                            <form action="./procesos_perfil/añadir_amigo.php" method="post">
+                                <input type="hidden" name="id" value="<?php echo $fila['id_usu']; ?>">
+                                <input type="submit" value="Añadir">
+                            </form>
+                        </td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    <?php elseif (!empty($searchTerm)) : ?>
+        <p>No se encontraron resultados para "<?php echo htmlspecialchars($searchTerm, ENT_QUOTES, 'UTF-8'); ?>"</p>
+    <?php endif; ?>
+
 <?php
 } catch (Exception $th) {
     // Manejo de excepciones de manera adecuada
